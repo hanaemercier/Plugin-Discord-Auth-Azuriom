2fde4673efe17aff8d13a8f6d81e09673efb8e5c
Hanaé | Enoya <51682613+lapetitehanae@users.noreply.github.com>
2023-06-08 00:26:09 +0200

    Add ukrainian language
    Translated by xlanyleeet

---
+++ resources/lang/uk/admin.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'nav' => [
+        'title' => 'Аутентифікація Discord',
+        'settings' => 'Налаштування',
+    ],
+
+    'permission' => 'Переглядати і змінювати аутентифікацію Discord',
+
+    'settings' => [
+        'title' => 'Налаштування аутентифікації Discord',
+        'header' => 'Керування автентифікацією через Discord',
+        'subtitle' => 'Ваш застосунок Discord',
+        'discord-portal' => 'Зберегти у застосунку Discord',
+        'info' => "У Discord Developers, в OAuth -> Redirects секції, не забудьте додати :",
+
+        'client_id' => 'Client ID',
+        'client_secret' => 'Client Secret',
+        'guild' => 'Guild ID (залиште пустим, якщо ви хочете дозволити користувачеві увійти, навіть якщо він не присутній на сервері вашої гільдії)',
+    ],
+];
